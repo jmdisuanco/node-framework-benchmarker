c95d462112ec01dad83935eb6e6395aa8f3933d8
JM <jm@disuan.co>
2019-02-27 23:37:23 +0400

    update benchmarker

--- frameworks/pinipig.js
+++ frameworks/pinipig.js
@@ -1,13 +1,15 @@
 const pinipig = require('pinipig')
-const stringify =  require('fast-stringify').default
 
-let routes = [
-    {
-        url:'/',
-        get: (ctx) => {
-            ctx.res.end( stringify({hello:'world'}) )
-        }
+let routes = [{
+    url: '/',
+    get: (ctx) => {
+        ctx.res.json({
+            hello: 'world'
+        })
     }
-]
+}]
 
-pinipig.createServer({port:9090,routes})
+pinipig.createServer({
+    port: 9090,
+    routes
+})
--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const {spawn} = require('child_process')
+const {
+    spawn
+} = require('child_process')
 const fs = require('fs')
 const autocannon = require('autocannon')
 
 
-let framework= fs.readdirSync('./frameworks')
+let framework = fs.readdirSync('./frameworks')
 let index = 0
-let max =Object.keys(framework).length
+let max = Object.keys(framework).length
 let report = []
-console.log(max)
-let start = (f)=>{
+console.log(`${max} frameworks to benchmark`)
+let start = (f) => {
 
     let child = spawn('node', [`./frameworks/${f}`])
 
-    child.stdout.on('data',  (data) => {
-    console.log(`starting ${f}`)
-     benchmark(child,f)
+    child.stdout.on('data', (data) => {
+        console.log(`starting ${f}`)
+        benchmark(child, f)
     });
-    
-    child.stderr.on('data',  (data) =>{
+
+    child.stderr.on('data', (data) => {
         console.log("Error");
         console.log('test: ' + data);
     });
-    
-    child.on('close',  (code) => {
+
+    child.on('close', (code) => {
         console.log("closing ", f);
-        if(index < max) { 
+        if (index < max) {
             start(framework[index])
-        }else{
-            console.log(report)
-            fs.writeFileSync('./report.json',JSON.stringify(report,null,2))
+        } else {
+            fs.writeFileSync('./report.json', JSON.stringify(report, null, 2))
+            console.log('result has been save at report.json')
         }
     })
 }
@@ -36,18 +38,18 @@ let start = (f)=>{
 
 start(framework[index])
 
-let benchmark = (child,f)=> {
-    console.log(child.pid)
-autocannon({
-    title:f,
-  url: 'http://localhost:9090',
-  connections: 10, //default
-  pipelining: 1, // default
-  duration: 10 // default
-}, endBechmark(child))
+let benchmark = (child, f) => {
+    console.log(`${index + 1} of ${max}`)
+    autocannon({
+        title: f,
+        url: 'http://localhost:9090',
+        connections: 100,
+        pipelining: 10,
+        duration: 5
+    }, endBechmark(child))
 }
 
-let endBechmark =(child)=> (err, result)=>{
+let endBechmark = (child) => (err, result) => {
     index += 1
     report.push(result)
     child.kill('SIGINT')
